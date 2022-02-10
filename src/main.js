4b13f22ea6815a41475371cd65522412d3236be5
camilamejiax <96745732+camilamejiax@users.noreply.github.com>
2022-02-10 10:40:09 -0500

    Added getForecast coordinates and response within displayTemperature

--- src/main.js
+++ src/main.js
@@ -96,7 +96,14 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
-displayForecast(response);
+getForecast(response.data.coord);
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "7474fc14b19a5b425bb49dcabc42f77b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function search(city) {
